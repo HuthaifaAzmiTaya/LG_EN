d5b8793ac140de66e0bd718cdc720911c0d4d5fe
HuthaifaAzmiTaya <huthaifazmi@hotmail.com>
2025-11-06 01:40:57 +0300

    exce analyzer cinfigs

--- config/initializers/ai_exception_analyzer.rb
+++ config/initializers/ai_exception_analyzer.rb
@@ -14,4 +14,24 @@ AiExceptionAnalyzer.configure do |config|
     config.github_token = ENV["AI_EXCEPTION_ANALYZER_GITHUB_TOKEN"]
     config.github_repository = ENV["AI_EXCEPTION_ANALYZER_GITHUB_REPOSITORY"]
 
+    # Analysis settings
+    config.analysis_timeout_seconds = ENV.fetch("AI_EXCEPTION_ANALYZER_TIMEOUT_SECONDS", "10").to_i
+    config.max_analysis_retries = ENV.fetch("AI_EXCEPTION_ANALYZER_MAX_RETRIES", "3").to_i
+    config.max_code_context_lines = ENV.fetch("AI_EXCEPTION_ANALYZER_MAX_CODE_CONTEXT_LINES", "5").to_i
+    config.max_stack_trace_depth = ENV.fetch("AI_EXCEPTION_ANALYZER_MAX_STACK_TRACE_DEPTH", "10").to_i
+
+    # Sensitive data patterns to scrub
+    config.sensitive_data_patterns = [
+        /Authorization:\s*Bearer\s+\S+/i, 
+        /api_key=\S+/i, 
+        /password=\S+/i, 
+        /password/i,
+        /token/i,
+        /api[_-]?key/i,
+        /secret/i,
+        /credit[_-]?card/i,
+        /ssn/i,
+        /(\d{3}[-.]?\d{3}[-.]?\d{4})/,
+        /([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})/,
+    ]
 end
